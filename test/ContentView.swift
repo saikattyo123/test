8d402017626fd19a275036dbaa096176296d7e2b
斎藤克哉 <katsuya12321@gmail.com>
2019-11-02 18:11:34 +0900

    Initial Commit

---
+++ test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test
+//
+//  Created by 斎藤克哉 on 2019/11/02.
+//  Copyright © 2019 PeerRadio. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
