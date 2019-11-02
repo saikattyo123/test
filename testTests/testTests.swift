15769c7337395bcc9c52412490007ad7e7f74cf4
斎藤克哉 <katsuya12321@gmail.com>
2019-11-02 19:04:21 +0900

    l

--- testTests/testTests.swift
+++ testTests/testTests.swift
@@ -22,6 +22,7 @@ class testTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertEqual("test", SceneDelegate.get(text: "test"))
     }
 
     func testPerformanceExample() {
